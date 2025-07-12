d4a5026f80b9e8dfc0588600a778730665afe46c
Hitesh11112 <hiteshpatwal2004@gmail.com>
2025-07-12 12:40:44 +0530

    added separate folder for solving question:

---
+++ file.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	int t = 1;
+	while (t--){
+		int n;
+		cin >> n;
+		for (int i = 0; i < n; ++i) {
+			cout << "Hello world " << i << " times." << endl;
+		}
+	}
+
+	return 0;
+}
