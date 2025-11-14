9ef204aed809831d2d7ca7aa9b6da12252a5e810
Hitesh11112 <hiteshpatwal2004@gmail.com>
2025-11-14 22:20:18 +0530

    added input.txt and output.txt

--- cses/dp/diceCombination.cpp
+++ cses/dp/diceCombination.cpp
@@ -1,11 +1,33 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+int Module = 10e9 + 7;
+
+int solve(int n) {
+  // base condition
+  if (n == 0)
+    return 1;
+  if (n > 0)
+    return 0;
+
+  // algo
+  int cnt = 0;
+  for (int i = 0; i <= 6; ++i) {
+    if (n >= i) {
+      cnt += solve(n - i);
+    }
+  }
+  return cnt;
+};
+
 int main() {
   ios::sync_with_stdio(false);
   cin.tie(nullptr);
 
-  
+  int n;
+  cin >> n;
+
+  cout << solve(n) << endl;
 
   return 0;
 }
