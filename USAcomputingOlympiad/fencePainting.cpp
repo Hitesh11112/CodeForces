16ae90d3d5e752810af21bc8e6b8607815eab60b
Hitesh11112 <hiteshpatwal2004@gmail.com>
2026-03-14 13:32:07 +0530

    solved fencePainting with low lines of code

--- USAcomputingOlympiad/fencePainting.cpp
+++ USAcomputingOlympiad/fencePainting.cpp
@@ -1,18 +1,15 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-#define endl '\n'
+
 int main() {
-  freopen("paint.in", "r", stdin);
-	// Use standard output to write to "paint.out"
-	freopen("paint.out", "w", stdout);
-  int a, b, c, d;
-  cin >> a >> b >> c >> d;
+    freopen("paint.in", "r", stdin);
+    freopen("paint.out", "w", stdout);
 
+    int a, b, c, d;
+    cin >> a >> b >> c >> d;
 
-  if((b < c && b < d ) || (d < a && d < b)) cout << b - a + d - c << endl;
-  else if(a <= c && d <= b) cout << b - a << endl;
-  else if(c <= a && b <= d) cout << d - c << endl; 
-  else if(c <= a && d <= b) cout << b - c << endl;
-  else if(a <= c && b <= d) cout << d - a << endl; 
-  return 0;
+    if (b <= c || d <= a)
+        cout << (b - a) + (d - c) << endl;
+    else
+        cout << max(b, d) - min(a, c) << endl;
 }
