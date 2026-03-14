e4a293382e2e41c1cd9b43b392382838a87cd5a9
Hitesh11112 <hiteshpatwal2004@gmail.com>
2026-03-14 13:27:25 +0530

    added folder USACO problem fencePainting.cpp

---
+++ contest/codeforces/round1062/c.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int test;
+  cin >> test;
+
+  while (test--) {
+    int n;
+    cin >> n;
+
+    vector<int> arr(n);
+
+    for (auto &i : arr) {
+      cin >> i;
+    }
+    
+  }
+
+  return 0;
+}
