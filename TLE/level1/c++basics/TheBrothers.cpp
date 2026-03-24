1dc4a0f7c33c5890d2f7c94bd994fb77af041780
Hitesh11112 <hiteshpatwal2004@gmail.com>
2026-03-24 21:36:40 +0530

    solved the brothers cf problem

---
+++ TLE/level1/c++basics/TheBrothers.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  string a, b, c, d;
+  cin >> a >> b >> c >> d;
+  if (b == d) {
+    cout << "ARE Brothers\n";
+  } else {
+    cout << "NOT\n";
+  }
+}
