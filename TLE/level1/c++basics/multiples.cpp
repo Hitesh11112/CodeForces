f717e2f9d0abcef4d8e9f5bf0eae108549ca3282
Hitesh11112 <hiteshpatwal2004@gmail.com>
2026-03-28 06:59:00 +0530

    sovled multiples cf problem

---
+++ TLE/level1/c++basics/multiples.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int a, b;
+  cin >> a >> b;
+  if (b > a)
+    swap(a, b);
+  if (a % b == 0)
+    cout << "Multiples\n";
+  else
+    cout << "No Multiples\n";
+}
